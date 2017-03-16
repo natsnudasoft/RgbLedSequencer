d73a1d75b9033f30fa2a444476720dc8016f2228
Adrian Dunstan <adunstan@fastmail.co.uk>
2017-03-16 19:16:02 +0000

    Target 4.6, update packages and development dependencies
    Target .NET Framework 4.6 instead of 4.6.2; this is a library and this allows us to support more versions of projects referencing this library.
    Added developmentDependency="true" to relevant packages.
    Updated codecrackers and ReportGenerator.

--- tools/run-test-report.bat
+++ tools/run-test-report.bat
@@ -1,7 +1,7 @@
 @echo off
 SET openCoverVersion=4.6.519
 SET xunitRunnerVersion=2.2.0
-SET reportGeneratorVersion=2.5.5
+SET reportGeneratorVersion=2.5.6
 ..\packages\OpenCover.%openCoverVersion%\tools\OpenCover.Console.exe -register:user "-filter:+[*]* -[*Tests]*" -target:..\packages\xunit.runner.console.%xunitRunnerVersion%\tools\xunit.console.exe -targetargs:"..\test\unit\RgbLedSequencerLibraryTests\bin\Debug\RgbLedSequencerLibraryTests.dll -noshadow" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*Designer.cs -output:coverage.xml && ^
 ..\packages\ReportGenerator.%reportGeneratorVersion%\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:coverage && ^
 echo Press any key to display report... && ^
